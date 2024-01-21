388e6c1b56876a78b7634adecb49eac10fc2172d
kyriakossk2000 <kyriakoskyriakou2000@gmail.com>
2024-01-21 21:32:02 +0200

    Done with modelling - updated EDA

--- modeling.py
+++ modeling.py
@@ -1,14 +1,12 @@
 import pandas as pd
 import numpy as np
 import argparse
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import MinMaxScaler
-import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 import plotly.graph_objects as go
-import datetime
-
 
 def str2bool(s):
     if s not in {'false', 'true'}:
@@ -18,10 +16,11 @@ def str2bool(s):
 # argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_path', required=True)
-parser.add_argument('--model_type', required=True, choices=['linear', 'forest', 'lstm'], help='Type of model to use')
+parser.add_argument('--model_type', required=True, choices=['linear', 'forest', 'mlp', 'all'], help='Type of model to use or all to choose all of them')
 parser.add_argument('--n_estimators', default=100, type=int)  # for Random Forest
-parser.add_argument('--epochs', default=10, type=int)  # for LSTM
-parser.add_argument('--timesteps', default=1, type=int, help='Number of timesteps for LSTM input')
+parser.add_argument('--epochs', default=30, type=int)  # for mlp
+parser.add_argument('--units_layers', default='500, 250, 500', type=str, help='Type units, layers of MLP.')  # for mlp
+parser.add_argument('--solver', default='adam', choices=['adam', 'lbfgs', 'sgd'], help='Type of solver for weight optimization in MLP.')  # for mlp
 parser.add_argument('--future_days', default=7, type=int, help='Predict for the next X days')
 parser.add_argument('--past_days', default=7, type=int, help='# of past days to plot')
 
@@ -43,26 +42,26 @@ def load_and_preprocess_data(data_path):
 
 def feature_engineering(data):
     try:
-        daily_data = data.resample('D').agg({
-        'ACTUAL_DELIVERY_MINUTES - ESTIMATED_DELIVERY_MINUTES': 'mean',   # Aggregate data to daily level
-        'ITEM_COUNT': 'sum',
-        'USER_LAT': 'mean',
-        'USER_LONG': 'mean',
-        'VENUE_LAT': 'mean',
-        'VENUE_LONG': 'mean',
-        'ESTIMATED_DELIVERY_MINUTES': 'mean',
-        'ACTUAL_DELIVERY_MINUTES': 'mean',
-        'CLOUD_COVERAGE': 'mean',
-        'TEMPERATURE': 'mean',
-        'WIND_SPEED': 'mean',
-        'PRECIPITATION': 'mean'
-        })
+        daily_data = data.resample('D').agg({  # based on EDA --> best features seem to be ITEM_COUNT, ESTIMATED_DELIVERY_MINUTES, ACTUAL_DELIVERY_MINUTES)
+        'ITEM_COUNT': 'sum', # total number of items ordered that day 
+        'ESTIMATED_DELIVERY_MINUTES': 'sum',  
+        'ACTUAL_DELIVERY_MINUTES': 'sum',
+        # 'WIND_SPEED': 'mean',
+        # 'ACTUAL_DELIVERY_MINUTES - ESTIMATED_DELIVERY_MINUTES': 'mean',
+        # 'USER_LAT': 'mean',
+        # 'USER_LONG': 'mean', # for geospatial data -> mean for central loc
+        # 'VENUE_LAT': 'mean',
+        # 'VENUE_LONG': 'mean',
+        # 'CLOUD_COVERAGE': 'mean', # for weather condition makes sense to have mean of the day
+        # 'TEMPERATURE': 'mean',
+        # 'PRECIPITATION': 'mean'
+         })
 
         # target variable
         daily_data['Order_Volume'] = data.resample('D').size() # count no of orders for each day
 
-        # Select features and target
-        features = daily_data.drop(columns=['Order_Volume'])
+        # select features and target
+        features = daily_data.drop(columns=['Order_Volume']) # could add here more to drop
         target = daily_data['Order_Volume']
 
         return daily_data, features, target 
@@ -74,7 +73,7 @@ def feature_engineering(data):
 # time-based split of the data
 def time_based_split(data, days_for_test, features, target):
     try:
-        split_date = data.index.max() - pd.Timedelta(days=days_for_test)
+        split_date = data.index.max() - pd.Timedelta(days=days_for_test)        
         X_train = features.loc[features.index <= split_date]
         X_test = features.loc[features.index > split_date]
         y_train = target.loc[target.index <= split_date]
@@ -84,74 +83,131 @@ def time_based_split(data, days_for_test, features, target):
         print(f"Error in time-based data split: {e}")
         raise
 
-def train_model(X_train, y_train):
-    try:
-        model = LinearRegression() # linear regression model fit
-        model.fit(X_train, y_train)
-        return model
-    except Exception as e:
-        print(f"Error in model training :{e}")
-        raise
+def train_models(X_train, y_train):
+    models = {}
+    scalers = {} 
 
-def eval_model(model, X_test, y_test):
-    try:
-        y_pred = model.predict(X_test)
-        mae = mean_absolute_error(y_test, y_pred)
-        mse = mean_squared_error(y_test, y_pred)
-        print(f"\nModel Evaluation:\nMean Absolute Error: {mae:.2f}\nMean Squared Error: {mse:.2f}")
+    if args.model_type in ['all', 'linear']:
+        models['linear'] = LinearRegression().fit(X_train, y_train) # fit linear reg
+    if args.model_type in ['all', 'forest']:
+        models['forest'] = RandomForestRegressor(n_estimators=args.n_estimators).fit(X_train, y_train) # fit random forest
+    if args.model_type in ['all', 'mlp']:
+        scaler_X = MinMaxScaler() # scaling features and target
+        scaler_y = MinMaxScaler()
+        X_train_scaled = scaler_X.fit_transform(X_train)
+        y_train_scaled = scaler_y.fit_transform(y_train.values.reshape(-1, 1)).ravel()
 
-    except Exception as e:
-        print(f"Error in model evaluation: {e}")
-        raise
+        mlp_hidden_layers = eval(args.units_layers)  
+        models['mlp'] = MLPRegressor(hidden_layer_sizes=mlp_hidden_layers, learning_rate='adaptive', max_iter=args.epochs, verbose=False, solver=args.solver).fit(X_train_scaled, y_train_scaled)
 
-def predict_future(X_test):
-    try:
-        future_dates = pd.date_range(start=X_test.index.max() + pd.Timedelta(days=1), periods=args.future_days, freq='D')
-        future_X = X_test.copy()  # use the last # days data as a proxy for future data
-        future_X.index = future_dates  # Assigning future dates to the data
-        predicted_data =  model.predict(X_test)
-        # Print actual and predicted order volumes for the test period
-        print("Comparing Actual and Predicted Order Volumes for the last 7 days of the test period:")
-        for actual, predicted, date in zip(y_test, predicted_data, X_test.index):
+        # Storing scalers for MLP
+        scalers['mlp'] = {'X': scaler_X, 'y': scaler_y}
+
+    return models, scalers
+
+def predict_future(models, X_test, scalers):
+    predictions = {}
+    future_dates = pd.date_range(start=X_test.index.max() + pd.Timedelta(days=1), periods=args.future_days, freq='D')
+
+    for model_name, model in models.items():
+        future_X = X_test.copy() # use the last # days data as a proxy for future data
+        future_X.index = future_dates # future dates to the data
+        if model_name == 'mlp':
+            # Use appropriate scaler for MLP
+            X_test_scaled = scalers['mlp']['X'].transform(X_test)
+            pred_scaled = model.predict(X_test_scaled)
+            pred = scalers['mlp']['y'].inverse_transform(pred_scaled.reshape(-1, 1)).ravel()
+            predictions[model_name] = pred
+        else:
+            predictions[model_name] = model.predict(future_X)
+        # message for actual and predicted order volumes for the test period
+        print(f"Comparing Actual and Predicted Order Volumes for the last 7 days of the test period for {model_name} model:")
+        for actual, predicted, date in zip(y_test, predictions[model_name], X_test.index):
             print(f"Date: {date.date()} - Actual: {actual:.0f}, Predicted: {predicted:.0f}")
-        return predicted_data
-    except Exception as e:
-        print(f"Error in predicting future order volume. Please check the prediction window!: {e}")
-        raise
+        print('\n')
+    return predictions
 
-
-def plot_predictions(y_train, y_test, predicted, future_dates, past_days=14):
-    # selecting most recent past_days data points from y_train
-    recent_train_dates = y_train.index[-past_days:]
+def plot_predictions(y_train, y_test, predictions, future_dates, past_days=14):
+    recent_train_dates = y_train.index[-past_days:]  # selecting recent past days from y_train
     recent_train_values = y_train.iloc[-past_days:]
 
     fig = go.Figure()
 
-    # plotting recent train data
-    fig.add_trace(go.Scatter(x=recent_train_dates, y=recent_train_values, mode='lines', name='Recent Train Data', line=dict(color='blue')))
+    # plot past train data
+    fig.add_trace(go.Scatter(
+        x=recent_train_dates, 
+        y=recent_train_values, 
+        mode='lines', 
+        name='Recent Train Data', 
+        line=dict(color='blue')
+    ))
 
-    # continue line with from the last point of y_train to the first point of y_test
-    combined_dates = recent_train_dates.tolist() + [y_test.index[0]]
-    combined_values = recent_train_values.tolist() + [y_test.iloc[0]]
-    fig.add_trace(go.Scatter(x=combined_dates, y=combined_values, mode='lines', name='Connecting Line', line=dict(color='blue')))   # plotting the connecting line
- 
-    # plotting actual future data as a continuous line
+    # connecting the last point of y_train to the first point of y_test with a line so we don't see gap
+    connecting_dates = [recent_train_dates[-1], y_test.index[0]]
+    connecting_values = [recent_train_values.iloc[-1], y_test.iloc[0]]
+    fig.add_trace(go.Scatter(
+        x=connecting_dates, 
+        y=connecting_values, 
+        mode='lines', 
+        name='Connecting Line',
+        line=dict(color='blue'),
+        showlegend=False,
+        hoverinfo='skip' # don't show this point as it's only for asthetic
+    ))
+
+    # plotting actual future data with different line
     actual_future_dates = y_test.index
-    fig.add_trace(go.Scatter(x=actual_future_dates, y=y_test, mode='lines+markers', name='Actual Future Data', line=dict(color='green')))
+    fig.add_trace(go.Scatter(
+        x=actual_future_dates, 
+        y=y_test, 
+        mode='lines+markers', 
+        name='Actual Future Data', 
+        line=dict(color='green')
+    ))
 
-    # plotting predicted data as separate markers (orange)
-    fig.add_trace(go.Scatter(x=future_dates, y=np.rint(predicted), mode='markers', name='Predicted', marker=dict(color='firebrick')))
+    # plotting predicted data from different models as markers
+    colors = ['firebrick', 'royalblue', 'gold', 'purple']  # different colors for different models
+    for (model_name, predicted), color in zip(predictions.items(), colors):
+        fig.add_trace(go.Scatter(
+            x=future_dates, 
+            y=np.rint(predicted), 
+            mode='markers', 
+            name=f'{model_name.capitalize()} predicted',
+            marker=dict(color=color)
+        ))
 
-    # Legend design
+    # updating layout of the figure
     fig.update_layout(
         title='Recent Train, Actual, and Predicted Order Volumes',
         xaxis_title='Date',
         yaxis_title='Order Volume',
         hovermode='x unified'
     )
-
     fig.show()
 
+def evaluate_models(models, X_test, y_test, scalers):
+    model_performance = {}
+    for model_name, model in models.items():
+        if model_name == 'mlp':
+            X_test_scaled = scalers['mlp']['X'].transform(X_test)
+            y_pred_scaled = model.predict(X_test_scaled)
+            y_pred = scalers['mlp']['y'].inverse_transform(y_pred_scaled.reshape(-1, 1)).ravel()
+        else:
+            y_pred = model.predict(X_test)
+        mae = mean_absolute_error(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        model_performance[model_name] = {'MAE': mae, 'MSE': mse}
+        print(f"{model_name.capitalize()} Model Evaluation:")
+        print(f"Mean Absolute Error: {mae:.2f}")
+        print(f"Mean Squared Error: {mse:.2f}\n")
+    return model_performance
+
+def compare_models(model_performance):
+    # --> best model based on lowest MSE
+    best_model = min(model_performance, key=lambda k: model_performance[k]['MSE'])
+    print(f"Best Model: {best_model.capitalize()}")
+    print(f"Performance: MAE = {model_performance[best_model]['MAE']:.2f}, MSE = {model_performance[best_model]['MSE']:.2f}")
+    print('\n')
 
 if __name__ == '__main__':
     data = load_and_preprocess_data(args.data_path) # load and preprocess data
@@ -159,12 +215,13 @@ if __name__ == '__main__':
     daily_data, features, target = feature_engineering(data)
 
     X_train, X_test, y_train, y_test = time_based_split(daily_data, args.future_days, features, target) # time-based split
-
-    model = train_model(X_train, y_train) # train 
-    eval_model(model, X_test, y_test) # evaluate
-    predicted_data = predict_future(X_test) # make predictions
-
-    plot_predictions(y_train, y_test, predicted_data, X_test.index, args.past_days)
-
-
+        
+    models, scalers = train_models(X_train, y_train) # train 
     
+
+    print("\n")
+    model_performance = evaluate_models(models, X_test, y_test, scalers) # evaluate models
+    compare_models(model_performance) # compare models
+
+    predictions = predict_future(models, X_test, scalers) # make future predictions
+    plot_predictions(y_train, y_test, predictions, X_test.index, args.past_days) # plot predictions
