f194ea4f527d7314c098b9055c34e2b2d8eb9dc9
kyriakossk2000 <kyriakoskyriakou2000@gmail.com>
2024-01-24 23:54:50 +0200

    statistical test, all option train, ensemble model

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Predicting Venue Popularity for Wolt
 
 ## Project Overview
-This project focuses on forecasting the number of orders Wolt might receive in the coming days. It aims to utilize historical order data to predict future order volumes, helping in resource allocation and planning.
+This project focuses on predicting venue popularity. It aims to utilize geospatial venue data points (longitude and latitude) and predict if a venue with that coordinates, will be Highly popular, Medium popular, or Low popular. This will help Wolt in resource allocation and planning.
 
 ## Folder Structure
 ```bash
@@ -25,20 +25,30 @@ pip install -r requirements.txt
 The `modeling.py` script is the main file that performs the modelling, training, and evaluation. It accepts several arguments to customize the training process.
 
 #### Example Commands
-Run the following command in the command line to execute the modelling script with the default parameters:
+Run the following command in the command line to execute the modelling script with all models:
 ```bash
-python modeling.py --data_path Data/orders_autumn_2020.csv --model_type svm
+python modeling.py --data_path Data\orders_autumn_2020.csv --model_type all --user_loc True
 ```
 To specify a model type and configure other parameters:
 ```bash
-python modeling.py --data_path Data/orders_autumn_2020.csv --model_type mlp --units_layers 500,250,500,500
+python modeling.py --data_path Data/orders_autumn_2020.csv --model_type mlp --user_loc True --learning_rate 0.01 --units_layers 500,250,500,500
 ```
+```bash
+python modeling.py --data_path Data/orders_autumn_2020.csv --model_type all --user_loc True
+```
+Many other hyper-parameters can be set. Here is another example of an ensemble model:
+```bash
+python modeling.py --data_path Data/orders_autumn_2020.csv --model_type ensemble --epochs 50 --degree 3 --n_estimators 50
+```
+Models available (model_type command): Multilayer Perceptron (mlp), Gradient Boosting Classifier (gb), Random Forest Classifier (forest), Naive Bayes (naive), Support Vector Classifier (svm), Ensemble model based on all (ensemble), All models run (all). When running 'all', a significance test will be executed to compare the models and check if there is a statistically significant difference.
 
 ## Data Analysis
 The `data_analysis.ipynb` notebook within the `Data` folder includes a comprehensive exploratory data analysis (EDA) of the dataset. Key insights are drawn through various visualizations.
 
 ## Task Description
-The task is to predict the daily order volume for the next `X` days (`future_days` argument) based on historical data. The model also plots the historical order volumes for the last `Y` days (`past_days` argument) alongside the predictions.
-- Adjust the `future_days` argument to set the forecast horizon.
-- Adjust the `past_days` argument to determine the number of historical days to plot.
-- This is a time-series forecasting analysis. 
+The task is to predict venue popularity based on geographic coordinates. The model also plots a popularity heatmap demonstrating areas that might be busy.
+- This is a classification task. Based on order volume of each coordinate, the task is to classify the venue into High, Medium, or Low popularity category.
+- Very close coordinates have been merged to one (round to three decimals).
+- Adjust the `user_loc` argument to use user geographic coordinates as features to the model.
+- Synthetic venue coordinates have been created to demonstrate model's generalization. It is expected that when these augmented points fall into 'popular' areas, will be classified into High category.  
+- Two HTML files are created that demonstrate the actual and predicted popularity of the venues. The **actual_popularity** shows all the venues in the dataset. The venues that belong to the test set are also displayed with blue markes. The **predicted_popularity**, presents only the venues from the test set. Red markers indicate misclassification, green ones present the correct classifications. 
--- modeling.py
+++ modeling.py
@@ -1,18 +1,18 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier, GradientBoostingClassifier
 from sklearn.metrics import classification_report
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 import folium
-from sklearn.ensemble import GradientBoostingClassifier
 from folium.plugins import HeatMap
 from geopy.distance import great_circle
 import random
 import argparse
-
+from scipy.stats import ttest_rel
+import numpy as np
 
 def str2bool(s):
     if s not in {'false', 'true'}:
@@ -22,13 +22,14 @@ def str2bool(s):
 # argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_path', required=True)
-parser.add_argument('--model_type', required=True, choices=['forest', 'naive', 'mlp', 'svm', 'gb'], help='Type of model to use')
+parser.add_argument('--model_type', required=True, choices=['forest', 'naive', 'mlp', 'svm', 'gb', 'ensemble', 'all'], help='Type of model to use')
 parser.add_argument('--n_estimators', default=100, type=int)  # for random forest and gradient boosting
 parser.add_argument('--kernel', default='rbf', type=str)  # for svm
 parser.add_argument('--degree', default=3, type=int)  # for svm
-parser.add_argument('--epochs', default=30, type=int)  # for mlp
+parser.add_argument('--epochs', default=100, type=int)  # for mlp
 parser.add_argument('--user_loc', default=False, type=bool)  # use user location as feature
-parser.add_argument('--units_layers', default='500, 250, 500', type=str, help='Type units, layers of MLP.')  # for mlp
+parser.add_argument('--units_layers', default='500, 250, 500, 500', type=str, help='Type units, layers of MLP.')  # for mlp
+parser.add_argument('--learning_rate', default=0.01, type=float)  # for mlp
 parser.add_argument('--solver', default='adam', choices=['adam', 'lbfgs', 'sgd'], help='Type of solver for weight optimization in MLP.')  # for mlp
 
 args = parser.parse_args()
@@ -77,33 +78,73 @@ def train_models(venue_data, features):
     scaler = StandardScaler()
     X_train_venue = scaler.fit_transform(X_train_venue)
     X_test_venue = scaler.transform(X_test_venue)
+    models = {} # save models
+    if args.model_type in ['all', 'ensemble']:
+        model1 = RandomForestClassifier(n_estimators=args.n_estimators, random_state=42)
+        model2 = MLPClassifier(hidden_layer_sizes=eval(args.units_layers), learning_rate='adaptive', max_iter=args.epochs, verbose=False, solver=args.solver)
+        model3 = SVC(kernel=args.kernel, degree=args.degree, probability=True)  # probability=True for SVC in ensemble
+        model4 = GaussianNB()
+        model5 = GradientBoostingClassifier(n_estimators=args.n_estimators)
 
-    if args.model_type == 'forest':
-        model = RandomForestClassifier(n_estimators=args.n_estimators, random_state=42) # training random forest classifier
-    elif args.model_type == 'mlp':
+        # create ensemble model
+        models['ensemble'] = VotingClassifier(estimators=[
+            ('rf', model1), ('mlp', model2), ('svm', model3), ('nb', model4), ('gb', model5)],
+            voting='soft').fit(X_train_venue, y_train_venue)  # soft voting for predicted probabilities
+        
+    if args.model_type in ['all', 'forest']:
+        models['forest'] = RandomForestClassifier(n_estimators=args.n_estimators, random_state=42).fit(X_train_venue, y_train_venue) # training random forest classifier
+    if args.model_type in ['all', 'mlp']:
         mlp_hidden_layers = eval(args.units_layers)  # read into tuple
-        model = MLPClassifier(hidden_layer_sizes=mlp_hidden_layers, learning_rate='adaptive', max_iter=args.epochs, verbose=False, solver=args.solver)  # train MLP
-    elif args.model_type == 'svm':
-        model = SVC(kernel=args.kernel, degree=args.degree) # train SVM
-    elif args.model_type == 'gb':
-        model = GradientBoostingClassifier(n_estimators=args.n_estimators)
-    else:
-        model = GaussianNB()  # train Naive Bayes 
-    model.fit(X_train_venue, y_train_venue)
+        models['mlp'] = MLPClassifier(hidden_layer_sizes=mlp_hidden_layers, learning_rate='adaptive', max_iter=args.epochs, verbose=False, solver=args.solver, learning_rate_init=args.learning_rate).fit(X_train_venue, y_train_venue)  # train MLP
+    if args.model_type in ['all', 'svm']:
+        models['svm'] = SVC(kernel=args.kernel, degree=args.degree).fit(X_train_venue, y_train_venue) # train SVM
+    if args.model_type in ['all', 'gb']:
+        models['gb'] = GradientBoostingClassifier(n_estimators=args.n_estimators).fit(X_train_venue, y_train_venue)
+    if args.model_type in ['all', 'naive']:
+        models['naive'] = GaussianNB().fit(X_train_venue, y_train_venue)  # train Naive Bayes 
+    
+    return models, X_test_venue, y_test_venue, scaler, train_indices, test_indices
 
-    return model, X_test_venue, y_test_venue, scaler, train_indices, test_indices
+# Significance test for models
+def significance_test(models, X_test, y_test, n_splits=5):
+    scores = {}
+    significant_diff_found = False
+
+    # cross validation scores for each model
+    for name, model in models.items():
+        cv_scores = cross_val_score(model, X_test, y_test, cv=n_splits)
+        scores[name] = cv_scores
+    
+    for model1 in models:   # comparing models
+        for model2 in models:
+            if model1 < model2:  # no repetitions
+                stat, p = ttest_rel(scores[model1], scores[model2])
+                print(f"Comparing {model1} with {model2}: p-value = {p}")
+                if p < 0.05:
+                    print(f"Significant difference between {model1} and {model2}")
+                    significant_diff_found = True
+
+    if not significant_diff_found:
+        print("No significant difference found between any of the models.")
+
+    return scores
+
 
 # Model Evaluation
-def eval_model(model, X_test_venue, y_test_venue):
-    print("\n")
-    print(f"Model Evaluation of {args.model_type.capitalize()}:")
-    y_pred = model.predict(X_test_venue)
-    classification_metrics_venue = classification_report(y_test_venue, y_pred) # generate report of results
-    print(classification_metrics_venue)
-    return y_pred
+def eval_model(models, X_test_venue, y_test_venue):
+    predictions = {}
+    for name, model in models.items():
+        if args.model_type in ['all', name]:
+            print(f"\nModel Evaluation of {name.capitalize()}:")
+            y_pred = model.predict(X_test_venue)
+            predictions[name] = y_pred
+            classification_metrics_venue = classification_report(y_test_venue, y_pred) # generate report of results
+            print(classification_metrics_venue)
+    return predictions
+
 
 # data augmentation and prediction
-def create_synthetic_venues(model, scaler, venue_data):
+def create_synthetic_venues(models, scaler, venue_data, model_type):
     # Helsinki city center coordinates --> (60.17, 24.94)
     lat_range = (60.155, 60.195)  # try to find best values for broad synthetic data 
     long_range = (24.925, 24.955)   
@@ -120,14 +161,18 @@ def create_synthetic_venues(model, scaler, venue_data):
     new_venues_df = pd.DataFrame(new_venue_data)
     new_venues_df_scaled = scaler.transform(new_venues_df)
 
-    new_venue_predictions = model.predict(new_venues_df_scaled)    # predict popularity tiers of new venues
+    selected_model = models.get(model_type, None)
+    if selected_model is None:
+        raise ValueError("Model type not found in trained models")
+
+    new_venue_predictions = selected_model.predict(new_venues_df_scaled)    # predict popularity tiers of new venues
     new_venues_df['predicted_popularity_tier'] = new_venue_predictions
 
     return new_venues_df
 
 
 # Plotting
-def plot_map(venue_data, train_indices, test_indices, y_pred, new_venues_df):
+def plot_map(venue_data, train_indices, test_indices, predictions, new_venues_df, model_name):
     # combine train and test sets for the actual popularity heatmap
     combined_set_df = pd.concat([venue_data.iloc[train_indices], venue_data.iloc[test_indices]])
 
@@ -158,7 +203,7 @@ def plot_map(venue_data, train_indices, test_indices, y_pred, new_venues_df):
 
     # predicted popularity heatmap with markers for correct and incorrect predictions
     test_set_df = venue_data.iloc[test_indices].copy()
-    test_set_df['predicted_popularity_tier'] = y_pred
+    test_set_df['predicted_popularity_tier'] = predictions[model_name]
     predicted_heatmap_data = [
         (row['VENUE_LAT_ROUNDED'], row['VENUE_LONG_ROUNDED'], intensity_mapping[row['predicted_popularity_tier']])
         for idx, row in test_set_df.iterrows()
@@ -199,10 +244,13 @@ if __name__ == '__main__':
     
     venue_data, features = feature_engineering(data)
     
-    model, X_test_venue, y_test_venue, scaler, train_indices, test_indices = train_models(venue_data, features)
+    models, X_test_venue, y_test_venue, scaler, train_indices, test_indices = train_models(venue_data, features)
 
-    y_pred = eval_model(model, X_test_venue, y_test_venue)
+    predictions = eval_model(models, X_test_venue, y_test_venue)
 
-    new_venues_df = create_synthetic_venues(model, scaler, venue_data)
+    chosen_model_type = args.model_type if args.model_type != 'all' else 'mlp'  # default to mlp
+    new_venues_df = create_synthetic_venues(models, scaler, venue_data, chosen_model_type)
     
-    plot_map(venue_data, train_indices, test_indices, y_pred, new_venues_df)
+    plot_map(venue_data, train_indices, test_indices, predictions, new_venues_df, chosen_model_type)
+
+    test_results = significance_test(models, X_test_venue, y_test_venue) if args.model_type == 'all' else None
